6038312f7ccfdee6711d0ee24b297362fac48642
k-wilmeth <kwilmeth@fanthreesixty.com>
2026-04-06 18:59:12 -0500

    eslint

--- pages/api/topics/[topicId].ts
+++ pages/api/topics/[topicId].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '@/db/dbConnect'
 import { TopicModel } from '@/db/models'
